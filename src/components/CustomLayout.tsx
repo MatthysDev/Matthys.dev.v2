e66b12540e43c1b2000947bfa386d8b2fdd3e971
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-12-09 01:49:17 +0100

    fix: 🐛 responsive menu with burger

--- src/components/CustomLayout.tsx
+++ src/components/CustomLayout.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { neonBlanc, neonPurple } from '@/components/neonStyles';
-import Menu from '@/components/Menu';
+import MenuMobile from '@/components/Menu/MenuMobile';
+import Menu from '@/components/Menu/Menu';
 
 type Props = {
     children: React.ReactNode;
@@ -10,66 +8,20 @@ type Props = {
 };
 
 const CustomLayout = ({ children, withMenu }: Props) => {
-    const router = useRouter();
-
-    const activeStyle = {
-        // Add your additional styles for the active link here
-        borderBottom: '2px solid red', // Example: a red underline
-    };
-
     return (
         <>
             <div className='fixed top-0 left-0 bottom-0 right-0 bg-[url("/grid.svg")] opacity-30 z-0'></div>
             <div className='w-full m-auto min-h-screen bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black'>
+                <div className='relative'>
+                    {/* Corrected the class name for "md:hidden" */}
+                    <div className='md:hidden'><MenuMobile /></div>
+                    {/* Corrected the class name for "md:visible" */}
+                    <div className='hidden md:block'><Menu /></div>
+                </div>
                 <div className='w-5/6 md:w-2/3 m-auto relative'>
-                    <Menu />
-                    {/* {withMenu && (
-                        <div className='flex m-auto p-8 justify-around '>
-                            <Link href='/about' legacyBehavior passHref>
-                                <a
-                                    className={`text-xl font-bold text-white relative m-auto ${router.pathname === '/' ? 'text-purple-300' : ''}`}
-                                    style={router.pathname === '/about' ? neonPurple : neonBlanc}
-                                >
-                                    About
-                                </a>
-                            </Link>
-                            <Link href='/posters' legacyBehavior passHref>
-                                <a
-                                    className={`text-xl font-bold text-white relative m-auto ${router.pathname === '/' ? 'text-purple-300' : ''}`}
-                                    style={router.pathname === '/posters' ? neonPurple : neonBlanc}
-                                >
-                                    Posters
-                                </a>
-                            </Link>
-                            <Link href='/' legacyBehavior passHref>
-                                <a
-                                    className={`text-3xl font-bold text-white relative m-auto ${router.pathname === '/' ? 'text-purple-300' : ''}`}
-                                    style={router.pathname === '/' ? neonPurple : neonBlanc}
-                                >
-                                    Matthys.Dev
-                                </a>
-                            </Link>
-                            <Link href='/video' legacyBehavior passHref>
-                                <a
-                                    className={`text-xl font-bold text-white relative m-auto ${router.pathname === '/' ? 'text-purple-300' : ''}`}
-                                    style={router.pathname === '/video' ? neonPurple : neonBlanc}
-                                >
-                                    Video
-                                </a>
-                            </Link>
-                            <Link href='/contact' legacyBehavior passHref>
-                                <a
-                                    className={`text-xl font-bold text-white relative m-auto ${router.pathname === '/' ? 'text-purple-300' : ''}`}
-                                    style={router.pathname === '/contact' ? neonPurple : neonBlanc}
-                                >
-                                    Contact
-                                </a>
-                            </Link>
-                        </div>
-                    )} */}
                     {children}
                 </div>
-            </div >
+            </div>
         </>
     );
 };
---
+++ src/components/Menu/MenuMobile.tsx
@@ -0,0 +1,119 @@
+import { neonBlanc, neonPurple } from '@/components/neonStyles';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react'
+import { motion } from "framer-motion"
+
+type Props = {}
+
+export default function MenuMobile({ }: Props) {
+    const router = useRouter();
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-white transition ease transform duration-300`;
+    const menuVariants = {
+        open: {
+            opacity: 1,
+            scale: 1,
+            y: 0,
+            x: 0, // Change this to 0 for the top right
+            transition: {
+                ease: 'easeInOut',
+                duration: 0.6, // Adjust the duration as needed
+            },
+        },
+        closed: {
+            opacity: 0,
+            scale: 0,
+            y: -300,
+            x: 300, // Change this to 300 for the top right
+            transition: {
+                ease: 'easeInOut',
+                duration: 0.6, // Adjust the duration as needed
+            },
+        },
+    };
+
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+    return (
+        <>
+            <div className='mx-4 md:hidden pt-4 flex justify-between text-white cursor-pointer ' >
+                <a
+                    className={`text-xl ml-1 mt-4 font-bold text-white my-auto ${router.pathname === '/' ? 'text-purple-300' : ''}`}
+                    style={router.pathname === '/about' ? neonPurple : neonBlanc}
+                >
+                    Matthys.dev
+                </a>
+                <button
+                    className="bg-gradient-to-tr from-gray-800 to-slate-950 fixed top-6 right-6 z-20 flex flex-col h-12 w-12 border border-white rounded justify-center items-center group"
+                    onClick={() => {
+                        setIsOpen(!isOpen);
+                        toggleMobileMenu();
+                    }}
+                >
+                    <div
+                        className={`${genericHamburgerLine} ${isOpen
+                            ? "rotate-45 translate-y-3 group-hover:opacity-100"
+                            : " group-hover:opacity-100"
+                            }`}
+                    />
+                    <div
+                        className={`${genericHamburgerLine} ${isOpen ? "opacity-0" : " group-hover:opacity-100"
+                            }`}
+                    />
+                    <div
+                        className={`${genericHamburgerLine} ${isOpen
+                            ? "-rotate-45 -translate-y-3  group-hover:opacity-100"
+                            : " group-hover:opacity-100"
+                            }`}
+                    />
+                </button>
+            </div>
+            <motion.div
+                initial="closed"
+                animate={isMobileMenuOpen ? 'open' : 'closed'}
+                variants={menuVariants}
+                className={`fixed top-0 left-0 w-full  z-10`}
+            >
+                <div className='mx-4 p-4 flex flex-col gap-4 mt-4 bg-gradient-to-tr from-gray-800 to-slate-950 rounded-lg shadow-[0px_0px_7px_1px_#fff] z-20 position'>
+                    <Link href='/about' passHref legacyBehavior>
+                        <a
+                            className={`text-xl font-bold text-white relative ${router.pathname === '/' ? 'text-purple-300' : ''}`}
+                            style={router.pathname === '/about' ? neonPurple : neonBlanc}
+                        >
+                            About
+                        </a>
+                    </Link>
+                    <Link href='/posters' legacyBehavior passHref>
+                        <a
+                            className={`text-xl font-bold text-white relative ${router.pathname === '/' ? 'text-purple-300' : ''}`}
+                            style={router.pathname === '/posters' ? neonPurple : neonBlanc}
+                        >
+                            Posters
+                        </a>
+                    </Link>
+                    <Link href='/video' legacyBehavior passHref>
+                        <a
+                            className={`text-xl font-bold text-white relative ${router.pathname === '/' ? 'text-purple-300' : ''}`}
+                            style={router.pathname === '/video' ? neonPurple : neonBlanc}
+                        >
+                            Video
+                        </a>
+                    </Link>
+                    <Link href='/contact' legacyBehavior passHref>
+                        <a
+                            className={`text-xl font-bold text-white relative ${router.pathname === '/' ? 'text-purple-300' : ''}`}
+                            style={router.pathname === '/contact' ? neonPurple : neonBlanc}
+                        >
+                            Contact
+                        </a>
+                    </Link>
+                </div>
+            </motion.div>
+
+        </>
+    )
+}
