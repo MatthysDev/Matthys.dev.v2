857ef2c11211a7fcbd8594f9b0031d915c9fef3f
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-12-26 18:55:37 +0100

    feat: 🎸 add picture

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,7 +11,10 @@ export default function about({ }: Props) {
             <div className='text-2xl text-white'>
                 Some pictures of me at different places
             </div>
-            <Image src={'/me/me-catalin.jpg'} alt={'Me and Catalin Miron at App.js 2023'} width={400} height={400} className='rounded-lg shadow-[0px_0px_20px_2px_#fff] my-12' />
+            <div className='flex gap-4 w-full'>
+                <Image src={'/me/me-catalin.jpg'} alt={'Me and Catalin Miron at App.js 2023'} width={400} height={400} className='rounded-lg shadow-[0px_0px_20px_2px_#fff] my-12' />
+                <Image src={'/me/amsterdam.jpg'} alt={'Me and David Leuliette at React Summit 2022'} width={400} height={400} className='rounded-lg shadow-[0px_0px_20px_2px_#fff] my-12' />
+            </div>
         </CustomLayout>
     )
 }
