aaa1e54e2354aa0755199d3476e31f84f2a0e62d
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2023-12-27 17:25:07 +0100

    feat: 🎸 add second video

--- src/apiData/videoApi.ts
+++ src/apiData/videoApi.ts
@@ -4,5 +4,11 @@ export const videoApi = [
         url: 'https://www.youtube.com/watch?v=pm2P0v8D0Hg',
         description: 'Casey neistat Bootcamp where i learned how to make videos',
         imgUrl: '/thumbnail/casey.jpg'
+    },
+    {
+        title: 'Numbr x Comet Talk',
+        url: 'https://www.youtube.com/watch?v=TpQA2kOlv-4',
+        description: 'Numbr x Comet Talk - Fullstack serverless app with Apollo, GraphQL, Hasura, Heroku, React Native',
+        imgUrl: '/thumbnail/gqlTalk.jpg'
     }
 ];
