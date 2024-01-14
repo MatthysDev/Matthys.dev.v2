4c2eeef315d6924e7d7c96edc23d6463f14f88bf
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-01-14 20:18:04 +0100

    feat: 🎸 new page : plan

---
+++ src/pages/plan.tsx
@@ -0,0 +1,12 @@
+import CustomLayout from '@/components/CustomLayout'
+import React from 'react'
+
+type Props = {}
+
+export default function plan({ }: Props) {
+    return (
+        <CustomLayout>
+            <div>2024 Plan</div>
+        </CustomLayout>
+    )
+}
