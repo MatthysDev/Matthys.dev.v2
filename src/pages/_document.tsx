b23091a65380efc356e3b7c263880d278ba93940
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-01-19 17:12:04 +0100

    fix: 🐛 responsive /video

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className='bg-slate-950'>
       <Head>
         <link rel="icon" href="/favicon.png" />
       </Head>
--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -14,32 +14,35 @@ const images = [
 export default function Video() {
     return (
         <CustomLayout withMenu>
-            <div className='flex gap-12'>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 w-1/2'>
-                    {images.map((image, index) => (
-                        <div key={index} className='relative rounded-lg overflow-hidden w-auto h-64'>
-                            <Image
-                                src={image.src}
-                                alt={image.alt}
-                                objectFit='cover'
-                                className='rounded-lg'
-                                fill
-                                loading='lazy'
-                            />
-                        </div>
-                    ))}
+            <div className='flex gap-12 mt-24 mb-32 2xl:my-32 '>
+                <div className='w-full md:w-1/2'>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+                        {images.map((image, index) => (
+                            <div key={index} className='relative rounded-lg overflow-hidden aspect-square'>
+                                <Image
+                                    src={image.src}
+                                    alt={image.alt}
+                                    objectFit='cover'
+                                    className='rounded-lg'
+                                    layout='responsive'
+                                    width={1} // Set width and height to maintain aspect ratio
+                                    height={1}
+                                    loading='lazy'
+                                />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-                <div className='w-1/2 flex flex-col gap-4'>
-                    <p className='text-white text-2xl font-bold'>Welcome to My Video Section</p>
-                    <p className='text-white text-lg'>Hey there! I‘m passionate about creating videos, and I‘m excited to share my journey with you.</p>
-                    <p className='text-white text-lg'>I recently got a new camera and I‘m in the process of learning how to use it. The world of filmmaking and video editing is both thrilling and challenging, and I can‘t wait to explore it further.</p>
-                    <p className='text-white text-lg'>On this channel, you‘ll find a mix of behind-the-scenes looks at my video-making process, tips and tricks I discover along the way, and, of course, the final results of my creative endeavors.</p>
-                    <p className='text-white text-lg'>While I may not be an expert just yet, I believe that the joy is in the journey. I‘ll be documenting my progress and sharing the highs and lows of my filmmaking experience, hoping it inspires fellow enthusiasts and beginners alike.</p>
-                    <p className='text-white text-lg'>So be ready to embark on this filmmaking adventure together. I can‘t wait to connect with you and build a community where we can learn and grow together!</p>
+                <div className='w-full md:w-1/2 flex flex-col gap-4'>
+                    <p className='text-white text-2xl font-bold 2xl:text-4xl mb-0 2xl:mb-4 '>Welcome to My Video Section</p>
+                    <p className='text-white text-md 2xl:text-2xl'>Hey there! I‘m passionate about creating videos, and I‘m excited to share my journey with you.</p>
+                    <p className='text-white text-lg 2xl:text-2xl'>I recently got a new camera and I‘m in the process of learning how to use it. The world of filmmaking and video editing is both thrilling and challenging, and I can‘t wait to explore it further.</p>
+                    <p className='text-white text-lg 2xl:text-2xl'>While I may not be an expert just yet, I believe that the joy is in the journey. I‘ll be documenting my progress and sharing the highs and lows of my filmmaking experience, hoping it inspires fellow enthusiasts and beginners alike.</p>
+                    <p className='text-white text-lg 2xl:text-2xl'>So be ready to embark on this filmmaking adventure together.</p>
                 </div>
             </div>
             <div>
-                <div className='text-3xl text-white mt-24 mb-12 font-bold'>Some of my inspirations</div>
+                <div className='text-3xl text-white pt-2 mb-12 font-bold'>Some of my inspirations</div>
                 <InspirationalVideos videos={inspirationalVideos} />
             </div>
         </CustomLayout>
