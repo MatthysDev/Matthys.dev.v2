84e905491d66de3ec789089a55d89edfcd322faf
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-01-25 15:05:43 +0100

    feat: 🎸 add clients on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,18 +4,24 @@ import Button from '@/components/Button';
 import Hero from '@/components/Hero';
 import { palette } from '@/utils/palette';
 import CustomLayout from '@/components/CustomLayout';
+import { Clients } from '@/components/Clients';
+import { clients } from '@/apiData/clients';
 
 export default function Home() {
 
   return (
     <CustomLayout withMenu>
       <div className="flex flex-col items-center p-0 md:p-12 text-white">
-        <Hero />
-        <div className='flex mt-0 mb-2 md:mt-24'>
+        <div className='mt-24'>
+          <Hero />
+        </div>
+        <div className='flex mt-0 mb-2'>
           <Button link={'/about'} text={'About me'} color={'bg-sky-200'} neonColor={palette.sky200} />
           <Button link={'https://airtable.com/appOrrPF35ppPjTKx/shrZLcDgGq0Kb6soN?backgroundColor=purple'} text={'Contact-me'} color={'bg-purple-300'} isExternal neonColor={palette.purple300} />
         </div>
       </div>
+      <div className='mt-24 text-xl font-bold text-center text-white md:text-4xl'>I work with them</div>
+      <Clients clients={clients} />
     </CustomLayout>
   )
 }
