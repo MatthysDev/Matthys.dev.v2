c333ea586bcddc5a9b4f42fe46c51f83e48306b1
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-02-01 18:33:46 +0100

    fix: 🐛 try to change img format and local path

--- src/apiData/posterApi.ts
+++ src/apiData/posterApi.ts
@@ -1,10 +1,10 @@
 export const posterData = [
-    { id: 1, title: "Shark", subtitle: "The Deep Blue Predator", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/shark.png", alt: "requin" },
-    { id: 2, title: "Bad Monkey", subtitle: "Mischief in the Jungle", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/bad-monkey.png", alt: "bad-monkey" },
-    { id: 3, title: "Fish", subtitle: "The Silent Swimmer", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/fish.png", alt: "fish" },
-    { id: 4, title: "Fox", subtitle: "Cunning of the Woods", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/fox.png", alt: "fox" },
-    { id: 5, title: "Good Monkey", subtitle: "Hero of the Trees", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/good-monkey.png", alt: "good-monkey" },
-    { id: 6, title: "Skeleton", subtitle: "Echoes of the Past", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/skeleton.png", alt: "skeleton" },
+    { id: 1, title: "Shark", subtitle: "The Deep Blue Predator", url: "/posters/shark.webp", alt: "requin" },
+    { id: 2, title: "Bad Monkey", subtitle: "Mischief in the Jungle", url: "/posters/bad-monkey.webp", alt: "bad-monkey" },
+    { id: 3, title: "Fish", subtitle: "The Silent Swimmer", url: "/posters/fish.webp", alt: "fish" },
+    { id: 4, title: "Fox", subtitle: "Cunning of the Woods", url: "/posters/fox.webp", alt: "fox" },
+    { id: 5, title: "Good Monkey", subtitle: "Hero of the Trees", url: "/posters/good-monkey.webp", alt: "good-monkey" },
+    { id: 6, title: "Skeleton", subtitle: "Echoes of the Past", url: "/posters/skeleton.webp", alt: "skeleton" },
     { id: 7, title: "Snake", subtitle: "Serpent's Embrace", url: "/posters/snake.webp", alt: "snake" },
-    { id: 8, title: "Frog", subtitle: "Leap of Faith", url: "https://raw.githubusercontent.com/MatthysDev/Matthys.Dev/main/src/images/posters/frog.png", alt: "frog" }
+    { id: 8, title: "Frog", subtitle: "Leap of Faith", url: "/posters/frog.webp", alt: "frog" }
 ];
