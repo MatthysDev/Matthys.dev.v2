e86094a11ff32b54d1e1104c0ba94789b2fd9f42
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-02-27 22:10:58 +0100

    feat: 🎸 add project api

--- src/apiData/projects.ts
+++ src/apiData/projects.ts
@@ -1,9 +1,50 @@
-
 export const projects = [
     {
-        githubUrl: "",
-        name: "",
-        description: "",
-        img: "",
+        websiteUrl: "https://dear-valentine.vercel.app/",
+        name: "DearValentine",
+        description: "A website to send love letters to your loved ones",
+        img: "❤️",
+    },
+    {
+        websiteUrl: "https//weshipit.today",
+        name: "weshipit.today",
+        description: "My company's website",
+        img: "🚀",
+    },
+    {
+        websiteUrl: "http://www.studio-lowcost.com/",
+        name: "Studio Low Cost",
+        description: "My mission was to add storybook and some components for the website",
+        img: "🎨",
+    },
+    {
+        websiteUrl: "https://ecv.digital",
+        name: "ECV School",
+        description: "My first bootcamp in a school as a teacher",
+        img: "📚",
+    },
+    {
+        websiteUrl: "https://www.deki.team/",
+        name: "Deki",
+        description: "Refactoring the mobile app with React Native and Typescript",
+        img: "📱",
+    },
+    {
+        websiteUrl: "https://drdata.io",
+        name: "DrData",
+        description: "Iinitiated the project and worked on the first version of the mobile app",
+        img: "📊",
+    },
+    {
+        websiteUrl: 'https://talibapp.com/',
+        name: 'Talib',
+        description: 'Migrate the mobile app from Javascript to Typescript',
+        img: '🤲'
+    },
+    {
+        websiteUrl: 'https://one-advice.vercel.app/',
+        name: 'One Advice',
+        description: 'A website to give your best advice',
+        img: '🤔'
     }
-]
+];
