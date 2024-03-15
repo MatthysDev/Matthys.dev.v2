4a250a28ce30811694fb63a726eac44d8da4d0f8
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-03-15 18:21:39 +0100

    fix: 🐛 responsive for portfolio

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -10,12 +10,12 @@ export default function Portfolio() {
             <Head>
                 <title>Matthys.dev | Portfolio</title> {/* Setting the title metadata */}
             </Head>
-            <div className='grid grid-cols-3'>
+            <div className='grid xl:grid-cols-3 grid-cols-1 md:grid-cols-2 mt-12 md:mt-0'>
                 {projects.map((project, index) => (
                     // eslint-disable-next-line react/jsx-key
                     <Card withNeon>
-                        <div className='flex flex-col gap-4 p-8'>
-                            <div className='flex gap-2 text-2xl font-bold'>
+                        <div className='flex flex-col gap-4 p-4 xl:pl-4 xl:py-6'>
+                            <div className='flex gap-2 text-xl xl:text-2xl font-bold'>
                                 <a href={project.websiteUrl}>
                                     {project.img}
                                 </a>
