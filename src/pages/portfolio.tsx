fe2ddc20beee378370fed8ab8ae9d2fb619a0896
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-03-17 22:17:58 +0100

    🎸 feat : add title for projects

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -5,12 +5,14 @@ import { projects } from '@/apiData/projects'
 import Card from '@/components/Cards/Card'
 
 export default function Portfolio() {
+    const year = new Date().getFullYear() - 2019
     return (
         <CustomLayout>
             <Head>
                 <title>Matthys.dev | Portfolio</title> {/* Setting the title metadata */}
             </Head>
             <div className='grid xl:grid-cols-3 grid-cols-1 md:grid-cols-2 mt-12 md:mt-0'>
+                <p>During the last {year} years, I have worked on a variety of projects. Here are some of them:</p>
                 {projects.map((project, index) => (
                     // eslint-disable-next-line react/jsx-key
                     <Card withNeon>
