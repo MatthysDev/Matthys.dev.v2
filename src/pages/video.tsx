c009fb721767d2bfc604158b0ef30d50849b8172
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2024-04-13 18:02:08 +0200

    🐛fix : add youtube link

--- src/pages/video.tsx
+++ src/pages/video.tsx
@@ -58,6 +58,9 @@ export default function Video() {
                 {/* <a href='https://www.tiktok.com/@mqtthys' target='_blank' rel='noreferrer'>
                     <div className='text-2xl pt-2 mb-4 font-bold'>Check out the Tiktok that i made</div>
                 </a> */}
+                <a href='https://www.tiktok.com/@mqtthys' target='_blank' rel='noreferrer'>
+                    <div className='text-2xl pt-2 mb-4 font-bold'>Check out my youtube channel</div>
+                </a>
             </div>
         </CustomLayout>
     );
