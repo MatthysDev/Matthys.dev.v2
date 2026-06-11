e031402040e8f5fc9fa744266133cfe358df9766
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2026-06-11 22:00:20 +0200

    🎸 feat : ajout clients Talib, Karnott, Shoootin, Human Coders, Filter Off

--- src/apiData/clients.ts
+++ src/apiData/clients.ts
@@ -4,5 +4,10 @@ export const clients = [
     { id: '2', data: { name: 'Deki', logo: { url: '/clients/deki.png' } } },
     { id: '3', data: { name: 'Studio Low Cost', logo: { url: '/clients/slc.png' } } },
     { id: '4', data: { name: 'ECV School', logo: { url: '/clients/ecv.png' } } },
-    { id: '5', data: { name: 'weshipit.today', logo: { url: '/clients/weshipit.png' } } }
+    { id: '5', data: { name: 'weshipit.today', logo: { url: '/clients/weshipit.png' } } },
+    { id: '6', data: { name: 'Talib', logo: { url: '/clients/talib.png' } } },
+    { id: '7', data: { name: 'Karnott', logo: { url: '/clients/karnott.png' } } },
+    { id: '8', data: { name: 'Shoootin', logo: { url: '/clients/shoootin.png' } } },
+    { id: '9', data: { name: 'Human Coders', logo: { url: '/clients/humancoders.png' } } },
+    { id: '10', data: { name: 'Filter Off', logo: { url: '/clients/filteroff.png' } } }
 ];
--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -16,7 +16,7 @@ export interface ClientsListHomepageProps {
 
 export function Clients(data: ClientsListHomepageProps) {
     return (
-        <div className="my-12 flex content-around gap-4 flex-col md:flex-row m-auto justify-center">
+        <div className="my-12 flex flex-wrap justify-center gap-4">
             {data.clients.map(
                 (client: ClientProps) =>
                     client.data && (
@@ -26,7 +26,8 @@ export function Clients(data: ClientsListHomepageProps) {
                                 alt={`Logo of ${client.data.name}`}
                                 width={250}
                                 height={250}
-                                className="h-32 w-32 opacity-60 grayscale transition duration-300 ease-in-out hover:opacity-100 hover:grayscale-0 rounded-3xl bg-white p-4 ring-1 ring-stone-900/10 dark:ring-white/10"
+                                title={client.data.name}
+                                className="h-24 w-24 opacity-60 grayscale transition duration-300 ease-in-out hover:opacity-100 hover:grayscale-0 rounded-2xl bg-white p-3 ring-1 ring-stone-900/10 dark:ring-white/10 md:h-28 md:w-28"
                             />
                         </div>
                     )
